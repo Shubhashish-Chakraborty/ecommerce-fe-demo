3e93dd561736f349fd635e5729f0add01f1e8375
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-02-24 06:35:12 +0530

    little more add ups

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,12 @@
 import { BrandsTape } from "../components/BrandsTape"
 import { Button } from "../components/ui/Button"
 
+import { useNavigate } from "react-router-dom"
+import { Arrow } from "../icons/Arrow";
+
 export const Home = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="min-h-screen bg-mainBg1">
             <div className="flex justify-around">
@@ -24,6 +29,24 @@ export const Home = () => {
                 </div>
             </div>
             <BrandsTape/>
+
+            <div>
+                <div className="font-bold text-3xl text-center mt-10 mb-8">Shop by Collection</div>
+
+                <div className="flex justify-center items-center cursor-pointer gap-x-20">
+                    <div>
+                        <img onClick={() => {navigate("/signup")}} src="menCollection.png" className="w-80" alt="" />
+                    </div>
+                    <div>
+                        <img onClick={() => {navigate("/signup")}} src="womenCollection.png" className="w-80" alt="" />
+                    </div>
+                </div>
+
+                <div className="flex justify-center mt-10">
+                    <Button onClick={() => {navigate("/signup")}} text="VIEW ALL PRODUCTS" variant="admin" endIcon={<Arrow/>}/>
+                </div>
+
+            </div>
         </div>
     )
 }
